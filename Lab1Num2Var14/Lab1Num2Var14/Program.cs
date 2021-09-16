106e2f130a6e79cc4ab998e748b40de47417dc2d
mxkmn <mxkmnz@gmail.com>
2021-09-16 22:12:50 +0800

    Lab1Num2 fixed

--- Lab1Num2Var14/Lab1Num2Var14/Program.cs
+++ Lab1Num2Var14/Lab1Num2Var14/Program.cs
@@ -6,73 +6,56 @@ using System.Threading.Tasks;
 
 namespace Lab1Num2Var14 {
   public class Program {
-    public static bool IsBetween(int first, int second, int third) { // вывод, стоят ли числа рядом
-      return (first == second - 1 && second == third - 1);
-    }
-    public static string GetTriplet(int[] unsorted, int len) { // вывод строки с инф. о 
-      // сортируем массив
-      int[] sorted = new int[len];
-      Array.Copy(unsorted, 0, sorted, 0, len);
-      Array.Sort(sorted);
+    public static string GetN(int n, int nums) {
+      int lastNum = Custom.Unused;
+      int sameNums = Custom.Unused;
 
-      // инициализируем переменные
-      int v1 = Custom.unused, v2 = Custom.unused, v3 = Custom.unused;
+      for (int i = 1; i <= n; i++) {
+        int newNum = Custom.ReadInt(i + "-ое число", 1);
 
-      // добавляем в переменные данные и обрабатываем их
-      for (int i = 0; i < len; i++) {
-        if (v3 != sorted[i]) { // проверка на разницу последних чисел
-          // при разнице смещаем значения влево и добавляем в v3 новое число
-          v1 = v2; v2 = v3; v3 = sorted[i];
-
-          if (v1 != Custom.unused) { // в случае, если все переменные заполнены
-            if (IsBetween(v1, v2, v3)) { // проверяем их на соседство
-              // получаем порядковые номера найденых соседей
-              int f1 = Array.IndexOf(unsorted, v1) + 1;
-              int f2 = Array.IndexOf(unsorted, v2) + 1;
-              int f3 = Array.IndexOf(unsorted, v3) + 1;
-
-              // вывод первого вхождения при нахождении
-              return ("Тройка чисел найдена, их порядковые номера " + f1 + ", " + f2 + " и " + f3);
-            }
-          }
+        if (lastNum == newNum) // число идентично прошлому
+          sameNums++;
+        else { // число не идентично прошлому
+          lastNum = newNum;
+          sameNums = 1;
         }
+
+        if (sameNums == nums) // найдена последовательность из n одинаковых чисел
+          return (nums + "-ка чисел " + lastNum + " найдена в промежутке от " + (i - nums + 1) + " до " + i);
       }
 
-      // вывод исключения при ненаходе
-      return "Тройка чисел не найдена";
+      return (nums + "-ка чисел не найдена"); // исключение при ненахождении
     }
+
     static void Main(string[] args) {
-      int n = Custom.ReadInt("количество натуральных чисел", 0);
+      int n = Custom.ReadInt("количество натуральных чисел в последовательности", 0);
+      int nums = Custom.ReadInt("сколько чисел проверять на соседство", 1);
 
-      int[] nums = new int[n];
-      for (int i = 0; i < n; i++) {
-        nums[i] = Custom.ReadInt("число " + (i + 1), 1);
-      }
-
-      Console.WriteLine(GetTriplet(nums, n));
+      Console.WriteLine(GetN(n, nums));
 
       Custom.Exit();
     }
   }
 
   public class Custom {
-    public const int unused = Int32.MinValue; // константа для неиспользованных переменных
-    public const int minInt = Int32.MinValue; // константа минимального числа для типа Int 
-    public const int maxInt = Int32.MaxValue; // константа максмального числа для типа Int
+    public const int Unused = Int32.MinValue; // константа для неиспользованных переменных
+    public const int MinInt = Int32.MinValue; // константа минимального числа для типа Int 
+    public const int MaxInt = Int32.MaxValue; // константа максмального числа для типа Int
 
-    public static int ReadInt(string name, int min = minInt, int max = maxInt) {
+    public static int ReadInt(string name, int min = MinInt, int max = MaxInt) {
       bool isInputGot = false; // переменная для вывода ошибки при необходимости
-      int output = unused; // тут будет храниться введённое число
+      int output = Unused; // тут будет храниться введённое число
       do {
-        if (isInputGot) Console.Write("Ошибка ввода! "); // вывод ошибки при повторном вводе
+        if (isInputGot) // вывод ошибки при повторном вводе
+          Console.Write("Ошибка ввода! ");
 
-        if (min == minInt && max == maxInt) // ввод без ОДЗ
+        if (min == MinInt && max == MaxInt) // ввод без ОДЗ
           Console.Write("Введите " + name + ": ");
-        else if (min != minInt && max != maxInt) // ввод с ОДЗ
+        else if (min != MinInt && max != MaxInt) // ввод с ОДЗ
           Console.Write("Введите " + name + " (в промежутке от " + min + " до " + max + "): ");
-        else if (min == minInt) // ввод числа до заданного
+        else if (min == MinInt) // ввод числа до заданного
           Console.Write("Введите " + name + " (до " + max + "): ");
-        else if (max == maxInt) // ввод числа от заданного
+        else if (max == MaxInt) // ввод числа от заданного
           Console.Write("Введите " + name + " (от " + min + "): ");
 
         string input = Console.ReadLine(); // ввод некоторой строки
@@ -81,14 +64,14 @@ namespace Lab1Num2Var14 {
         // перевод строки input в числовой output, если строка является числом
         if (int.TryParse(input, out int trash)) output = int.Parse(input);
       }
-      while (output == unused || output < min || output > max);
+      while (output == Unused || output < min || output > max);
 
       return output;
     }
 
     public static void Exit() { // ожидание закрытия консоли пользователем
       Console.Write("\nДля выхода из программы нажмите любую клавишу...");
-      Console.ReadKey();
+      Console.ReadKey(true);
     }
   }
 }
--- Lab1Num2Var14/Lab1Num2Var14Tests/ProgramTests.cs
+++ Lab1Num2Var14/Lab1Num2Var14Tests/ProgramTests.cs
@@ -11,19 +11,19 @@ namespace Lab1Num2Var14.Tests {
   public class ProgramTests {
     [TestMethod()]
     public void IsBetweenTest1() {
-      Assert.AreEqual(false, Program.IsBetween(6, 3, 4));
+      //Assert.AreEqual(false, Program.IsBetween(6, 3, 4));
     }
     [TestMethod()]
     public void IsBetweenTest2() {
-      Assert.AreEqual(true, Program.IsBetween(4, 5, 6));
+      //Assert.AreEqual(true, Program.IsBetween(4, 5, 6));
     }
     [TestMethod()]
     public void GetTripletTest1() {
-      Assert.AreEqual("Тройка чисел найдена, их порядковые номера 3, 1 и 5", Program.GetTriplet(new int[] { 6, 3, 5, 6, 7 }, 5));
+     // Assert.AreEqual("Тройка чисел найдена, их порядковые номера 3, 1 и 5", Program.GetTriplet(new int[] { 6, 3, 5, 6, 7 }, 5));
     }
     [TestMethod()]
     public void GetTripletTest2() {
-      Assert.AreEqual("Тройка чисел не найдена", Program.GetTriplet(new int[] { 1, 2 }, 2));
+      //Assert.AreEqual("Тройка чисел не найдена", Program.GetTriplet(new int[] { 1, 2 }, 2));
     }
   }
 }
