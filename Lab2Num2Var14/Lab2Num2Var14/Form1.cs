2998176441bc74b363171c96957c024c0e4b73e5
mxkmn <mxkmnz@gmail.com>
2021-09-28 00:53:30 +0800

    Накидал вёрстку

---
+++ Lab2Num2Var14/Lab2Num2Var14/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2Num2Var14 {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+    }
+    private void nums_OnChange() {
+
+    }
+
+    private void nums_KeyDown(object sender, KeyEventArgs e) {
+      nums_OnChange();
+    }
+
+    private void nums_TextChanged(object sender, EventArgs e) {
+      nums_OnChange();
+    }
+  }
+}
