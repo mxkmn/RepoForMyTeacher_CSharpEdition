4c9b550d66f96ba8552573b3b38eaa9fe90591cf
mxkmn <mxkmnz@gmail.com>
2021-09-27 23:25:09 +0800

    Подправил вёрстку, добавил простой метод проверки изменений

--- Lab2Num1Var12/Lab2Num1Var12/Form1.Designer.cs
+++ Lab2Num1Var12/Lab2Num1Var12/Form1.Designer.cs
@@ -25,72 +25,75 @@ namespace Lab2Num1Var12 {
     /// </summary>
     private void InitializeComponent() {
       this.label1 = new System.Windows.Forms.Label();
-      this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-      this.label2 = new System.Windows.Forms.Label();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+      this.numData = new System.Windows.Forms.NumericUpDown();
+      this.outLabel = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.numData)).BeginInit();
       this.SuspendLayout();
       // 
       // label1
       // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
       this.label1.AutoSize = true;
       this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
       this.label1.Location = new System.Drawing.Point(9, 9);
-      this.label1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+      this.label1.Margin = new System.Windows.Forms.Padding(0);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(143, 25);
       this.label1.TabIndex = 1;
       this.label1.Text = "Введите число:";
-      this.label1.Click += new System.EventHandler(this.label1_Click);
       // 
-      // numericUpDown1
+      // numData
       // 
-      this.numericUpDown1.AutoSize = true;
-      this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-      this.numericUpDown1.Location = new System.Drawing.Point(155, 7);
-      this.numericUpDown1.Maximum = new decimal(new int[] {
+      this.numData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.numData.AutoSize = true;
+      this.numData.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.numData.Location = new System.Drawing.Point(155, 7);
+      this.numData.Maximum = new decimal(new int[] {
             9999,
             0,
             0,
             0});
-      this.numericUpDown1.Minimum = new decimal(new int[] {
+      this.numData.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-      this.numericUpDown1.Name = "numericUpDown1";
-      this.numericUpDown1.Size = new System.Drawing.Size(120, 32);
-      this.numericUpDown1.TabIndex = 2;
-      this.numericUpDown1.Value = new decimal(new int[] {
+      this.numData.Name = "numData";
+      this.numData.Size = new System.Drawing.Size(120, 32);
+      this.numData.TabIndex = 2;
+      this.numData.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
+      this.numData.ValueChanged += new System.EventHandler(this.numData_Changed);
       // 
-      // label2
+      // outLabel
       // 
-      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+      this.outLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-      this.label2.Location = new System.Drawing.Point(0, 65);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(287, 30);
-      this.label2.TabIndex = 3;
-      this.label2.Text = "1 копейка";
-      this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-      this.label2.Click += new System.EventHandler(this.label2_Click);
+      this.outLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.outLabel.Location = new System.Drawing.Point(0, 65);
+      this.outLabel.Name = "outLabel";
+      this.outLabel.Size = new System.Drawing.Size(287, 30);
+      this.outLabel.TabIndex = 3;
+      this.outLabel.Text = "1 копейка";
+      this.outLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(289, 101);
-      this.Controls.Add(this.label2);
-      this.Controls.Add(this.numericUpDown1);
+      this.Controls.Add(this.outLabel);
+      this.Controls.Add(this.numData);
       this.Controls.Add(this.label1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
       this.Name = "Form1";
       this.Text = "Стоимость товара";
-      this.Load += new System.EventHandler(this.Form1_Load);
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numData)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -98,8 +101,8 @@ namespace Lab2Num1Var12 {
 
     #endregion
     private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.NumericUpDown numericUpDown1;
-    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.NumericUpDown numData;
+    private System.Windows.Forms.Label outLabel;
   }
 }
 
--- Lab2Num1Var12/Lab2Num1Var12/Form1.cs
+++ Lab2Num1Var12/Lab2Num1Var12/Form1.cs
@@ -13,17 +13,8 @@ namespace Lab2Num1Var12 {
     public Form1() {
       InitializeComponent();
     }
-
-    private void Form1_Load(object sender, EventArgs e) {
-
-    }
-
-    private void label1_Click(object sender, EventArgs e) {
-
-    }
-
-    private void label2_Click(object sender, EventArgs e) {
-
+    private void numData_Changed(object sender, EventArgs e) {
+      outLabel.Text = Convert.ToString(this.numData.Value);
     }
   }
 }
