d95a4e60f96276178d2e0f865414ac9bef812dd5
mxkmn <mxkmnz@gmail.com>
2021-09-27 23:11:05 +0800

    Накидал вёрстку

---
+++ Lab2Num1Var12/Lab2Num1Var12/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Lab2Num1Var12 {
+  partial class Form1 {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.label1 = new System.Windows.Forms.Label();
+      this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+      this.label2 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label1.Location = new System.Drawing.Point(9, 9);
+      this.label1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(143, 25);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Введите число:";
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // numericUpDown1
+      // 
+      this.numericUpDown1.AutoSize = true;
+      this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.numericUpDown1.Location = new System.Drawing.Point(155, 7);
+      this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+      this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+      this.numericUpDown1.Name = "numericUpDown1";
+      this.numericUpDown1.Size = new System.Drawing.Size(120, 32);
+      this.numericUpDown1.TabIndex = 2;
+      this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+      // 
+      // label2
+      // 
+      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label2.Location = new System.Drawing.Point(0, 65);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(287, 30);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "1 копейка";
+      this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.label2.Click += new System.EventHandler(this.label2_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(289, 101);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.numericUpDown1);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Стоимость товара";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.NumericUpDown numericUpDown1;
+    private System.Windows.Forms.Label label2;
+  }
+}
+
