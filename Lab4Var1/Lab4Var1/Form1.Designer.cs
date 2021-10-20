8b8d22726f09ba4b7ee3be3d3a0fb946bed8ea7f
mxkmn <mxkmnz@gmail.com>
2021-10-20 15:17:12 +0800

    4 лаба + отчёты
    В актуальные отчёты добавлены автотесты
    Создана 4 лаба, накидал интерфейс

---
+++ Lab4Var1/Lab4Var1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Lab4Var1 {
+  partial class Form1 {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.newDrink = new System.Windows.Forms.PictureBox();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.pictureBox0 = new System.Windows.Forms.PictureBox();
+      this.pictureBox3 = new System.Windows.Forms.PictureBox();
+      this.pictureBox4 = new System.Windows.Forms.PictureBox();
+      this.pictureBox5 = new System.Windows.Forms.PictureBox();
+      this.pictureBox2 = new System.Windows.Forms.PictureBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.buyDrink = new System.Windows.Forms.Button();
+      this.getDrink = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.newDrink)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // newDrink
+      // 
+      this.newDrink.BackColor = System.Drawing.Color.WhiteSmoke;
+      this.newDrink.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.newDrink.Location = new System.Drawing.Point(80, 423);
+      this.newDrink.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+      this.newDrink.Name = "newDrink";
+      this.newDrink.Size = new System.Drawing.Size(150, 150);
+      this.newDrink.TabIndex = 0;
+      this.newDrink.TabStop = false;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+      this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.pictureBox1.Location = new System.Drawing.Point(315, 348);
+      this.pictureBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+      this.pictureBox1.TabIndex = 1;
+      this.pictureBox1.TabStop = false;
+      // 
+      // label1
+      // 
+      this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.label1.ForeColor = System.Drawing.SystemColors.Control;
+      this.label1.Location = new System.Drawing.Point(17, 17);
+      this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(360, 50);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "Газированная";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // label2
+      // 
+      this.label2.Font = new System.Drawing.Font("Rockwell Nova Cond", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.label2.ForeColor = System.Drawing.SystemColors.Control;
+      this.label2.Location = new System.Drawing.Point(17, 43);
+      this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(360, 99);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "вода";
+      this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // pictureBox0
+      // 
+      this.pictureBox0.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox0.BackgroundImage")));
+      this.pictureBox0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.pictureBox0.Location = new System.Drawing.Point(246, 410);
+      this.pictureBox0.Name = "pictureBox0";
+      this.pictureBox0.Size = new System.Drawing.Size(127, 133);
+      this.pictureBox0.TabIndex = 4;
+      this.pictureBox0.TabStop = false;
+      // 
+      // pictureBox3
+      // 
+      this.pictureBox3.BackColor = System.Drawing.Color.WhiteSmoke;
+      this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.pictureBox3.Location = new System.Drawing.Point(315, 220);
+      this.pictureBox3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+      this.pictureBox3.Name = "pictureBox3";
+      this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+      this.pictureBox3.TabIndex = 5;
+      this.pictureBox3.TabStop = false;
+      // 
+      // pictureBox4
+      // 
+      this.pictureBox4.BackColor = System.Drawing.Color.WhiteSmoke;
+      this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.pictureBox4.Location = new System.Drawing.Point(315, 156);
+      this.pictureBox4.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+      this.pictureBox4.Name = "pictureBox4";
+      this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+      this.pictureBox4.TabIndex = 6;
+      this.pictureBox4.TabStop = false;
+      // 
+      // pictureBox5
+      // 
+      this.pictureBox5.BackColor = System.Drawing.Color.WhiteSmoke;
+      this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.pictureBox5.Location = new System.Drawing.Point(315, 92);
+      this.pictureBox5.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+      this.pictureBox5.Name = "pictureBox5";
+      this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+      this.pictureBox5.TabIndex = 7;
+      this.pictureBox5.TabStop = false;
+      // 
+      // pictureBox2
+      // 
+      this.pictureBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+      this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.pictureBox2.Location = new System.Drawing.Point(315, 284);
+      this.pictureBox2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+      this.pictureBox2.Name = "pictureBox2";
+      this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+      this.pictureBox2.TabIndex = 8;
+      this.pictureBox2.TabStop = false;
+      // 
+      // label3
+      // 
+      this.label3.ForeColor = System.Drawing.SystemColors.Control;
+      this.label3.Location = new System.Drawing.Point(17, 202);
+      this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(282, 68);
+      this.label3.TabIndex = 9;
+      this.label3.Text = "Количество доступных напитков: n";
+      this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // buyDrink
+      // 
+      this.buyDrink.Location = new System.Drawing.Point(22, 366);
+      this.buyDrink.Name = "buyDrink";
+      this.buyDrink.Size = new System.Drawing.Size(277, 47);
+      this.buyDrink.TabIndex = 10;
+      this.buyDrink.Text = "Купить напиток";
+      this.buyDrink.UseVisualStyleBackColor = true;
+      // 
+      // getDrink
+      // 
+      this.getDrink.Enabled = false;
+      this.getDrink.Location = new System.Drawing.Point(22, 583);
+      this.getDrink.Name = "getDrink";
+      this.getDrink.Size = new System.Drawing.Size(277, 47);
+      this.getDrink.TabIndex = 11;
+      this.getDrink.Text = "Забрать напиток";
+      this.getDrink.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.Firebrick;
+      this.ClientSize = new System.Drawing.Size(394, 651);
+      this.Controls.Add(this.getDrink);
+      this.Controls.Add(this.buyDrink);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.pictureBox2);
+      this.Controls.Add(this.pictureBox5);
+      this.Controls.Add(this.pictureBox4);
+      this.Controls.Add(this.pictureBox3);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.pictureBox0);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.newDrink);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+      this.Name = "Form1";
+      this.Text = "Автомат с напитками";
+      ((System.ComponentModel.ISupportInitialize)(this.newDrink)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox newDrink;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.PictureBox pictureBox0;
+    private System.Windows.Forms.PictureBox pictureBox3;
+    private System.Windows.Forms.PictureBox pictureBox4;
+    private System.Windows.Forms.PictureBox pictureBox5;
+    private System.Windows.Forms.PictureBox pictureBox2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Button buyDrink;
+    private System.Windows.Forms.Button getDrink;
+  }
+}
+
